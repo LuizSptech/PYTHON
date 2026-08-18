4913f66fb9ae3729ce4c00cb701eab0a1344dcc5
LuizSptech <luiz.correia@sptech.school>
2026-08-17 21:29:18 -0300

    progresso atividade

--- Aula2708/Atividadecsv.py
+++ Aula2708/Atividadecsv.py
@@ -2,23 +2,24 @@ import csv;
 import psutil;
 import time;
 
-tempo = time.localtime()
+tempo = time.ctime()
 Ram = psutil.virtual_memory()
 memoriaRAM = ((Ram.total-Ram.available)/ Ram.total * 100) ;
 frequencia = psutil.cpu_freq(percpu=False)
 disco = psutil.disk_usage('/')
 
-def tlg():
-    for i in range(5):
-        print(tempo[i])
-
-tlg()
-mensagemCPU = (f"frequencia da cpu {frequencia[0]}")
-mensagemRAM = (f"uso de memoria ram {memoriaRAM}")
-mensagemDISCO = (f"uso do disco {disco[1]}")
 
 
-#with open('./frequencia.csv','w') as csvfile:
- #   csv.writer(csvfile).writerow([mensagemCPU])
-  #  csv.writer(csvfile).writerow([mensagemRAM])
-   # csv.writer(csvfile).writerow([mensagemDISCO])
+
+horario_captura = tempo
+mensagemCPU = (f"{round(frequencia[0],2)}%")
+mensagemRAM = (f"{round(memoriaRAM,2)}%")
+mensagemDISCO = (f"{round(disco[1],2)}%")
+
+
+with open('./frequencia.csv','w') as csvfile:
+    csv.writer(csvfile, delimiter=";")
+    csv.writer(csvfile).writerow(["timestamp",horario_captura])
+    csv.writer(csvfile).writerow(["cpu",mensagemCPU])
+    csv.writer(csvfile).writerow(["ram",mensagemRAM])
+    csv.writer(csvfile).writerow(["disco",mensagemDISCO])
